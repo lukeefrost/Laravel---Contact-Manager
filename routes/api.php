4f31331888e849db8c840196680e5d19cb476baf
lukelliotfrost <luke@radio.co>
2020-05-31 19:02:16 +0100

    Created api routes.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Contact;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,34 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('contacts', function() {
+    return Contact::latest()->orderBy('created_at', 'desc')->get();
+});
+
+Route::get('contact/{id}', function($id) {
+    return Contact::findOrFail($id);
+});
+
+Route::post('contact/store', function(Request $request) {
+    return Contact::create([
+      'name' => $request->input('name');
+      'email' => $request->input('email');
+      'phone' => $request->input('phone');
+    ]);
+});
+
+Route::patch('contact/{id}', function(Request $request, $id) {
+    Contact::findOrFail($id)->update([
+      'name' => $request->input('name');
+      'email' => $request->input('email');
+      'phone' => $request->input('phone');
+    ]);
+});
+
+Route::delete('contact/{id}', function($id) {
+    return Contact::destroy($id);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
